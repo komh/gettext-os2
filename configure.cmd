894dc954e06231a413cf121047457cff11e5933e
KO Myung-Hun <komh78@gmail.com>
2026-03-10 20:58:32 +0900

    my: update configure.cmd
        modified:   configure.cmd

--- configure.cmd
+++ configure.cmd
@@ -1,15 +1,19 @@
 extproc sh
 
+d=$(dirname "$0" | tr '\\' /)
+
 n=configure
-test -f "./$n." || { echo "\`./$n' not found !!!"; exit 1; }
+test -f "$d/$n." || { echo "\`$d/$n' not found !!!"; exit 1; }
 
 export LDFLAGS=-Zhigh-mem
 
 opts="
+  --prefix=/@unixroot/usr/local
   --enable-shared
   --enable-static
   --disable-relocatable
   --with-included-gettext
 "
 
-"./$n" $opts "$@" 2>&1 | tee "$n.log"
+"$d/$n" $opts "$@" 2>&1 | tee "$n.log"
+exit ${PIPESTATUS[0]}
